a0f38c000cc4560a53b83b37216566dc8fe07abd
Corey Struzan <corey.struzan@gmail.com>
2016-03-15 10:24:38 -0700

    Specified generator in cmake_generate.bat to fix CMake error.

--- cmake_generate.bat
+++ cmake_generate.bat
@@ -1,2 +1,2 @@
 echo Running cmake...
-cmake -DBOOST_ROOT:STRING=C:\Boost -DBOOST_VER:STRING=1.55.0 ./
+cmake -G "Visual Studio 12 2013 Win64" -DBOOST_ROOT:STRING=C:\Boost -DBOOST_VER:STRING=1.55.0 ./
