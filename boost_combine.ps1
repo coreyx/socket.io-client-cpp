93bd5023e722a08f3d7b9968dd05f98c57d82880
Corey Struzan <corey.struzan@gmail.com>
2016-03-17 17:19:33 -0700

    Changed Powershell make directory commands to create directory rather than absolute file path

--- boost_combine.ps1
+++ boost_combine.ps1
@@ -1,13 +1,15 @@
 
 $boost_dir = "C:\Boost"
-$boost_lib_dir = $boost_dir + "\lib"
+$boost_lib_dir = $boost_dir + '\' + "lib"
 $boost_debug_dir = $boost_lib_dir + '\' + "debug"
 $boost_release_dir = $boost_lib_dir + '\' + "release"
 $boost_lib_filename = "boost.lib"
 $boost_debug_path = $boost_debug_dir + '\' + $boost_lib_filename
 $boost_release_path = $boost_release_dir + '\' + $boost_lib_filename
-$boost_debug_output_path = $env:APPVEYOR_BUILD_FOLDER + "\release\" + $boost_lib_filename
-$boost_release_output_path = $env:APPVEYOR_BUILD_FOLDER + "\debug\" + $boost_lib_filename
+$boost_debug_output_dir = $env:APPVEYOR_BUILD_FOLDER + '\' + "debug"
+$boost_release_output_dir = $env:APPVEYOR_BUILD_FOLDER + '\' + "release"
+$boost_debug_output_path = $boost_debug_output_dir + '\' + $boost_lib_filename
+$boost_release_output_path = $boost_release_output_dir + '\' + $boost_lib_filename
 
 # Original batch file converted to Powershell:
 # call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
@@ -44,10 +46,10 @@ New-Item -Force -ItemType directory -Path $boost_release_dir
 & lib.exe /OUT:debug\boost.lib *mt-gd-1_*.lib
 & lib.exe /OUT:release\boost.lib *mt-1_*.lib
 
-# md $boost_debug_output_path
-New-Item -Force -ItemType directory -Path $boost_debug_output_path
-# md $boost_release_output_path
-New-Item -Force -ItemType directory -Path $boost_release_output_path
+# md $boost_debug_output_dir
+New-Item -Force -ItemType directory -Path $boost_debug_output_dir
+# md $boost_release_output_dir
+New-Item -Force -ItemType directory -Path $boost_release_output_dir
 
 Copy-Item -Path $boost_debug_path -Destination $boost_debug_output_path
 Copy-Item -Path $boost_release_path -Destination $boost_release_output_path
