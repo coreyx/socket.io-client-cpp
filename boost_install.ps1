ac8190d3849a461817e878badb0f8bc39d27b453
Corey Struzan <corey.struzan@gmail.com>
2016-03-21 15:16:56 -0700

    Updating build scripts boost_install.ps1 and cmake_generate.ps1 to download, install and utilize latest version of Boost libraries (1.60)

--- boost_install.ps1
+++ boost_install.ps1
@@ -4,9 +4,9 @@
 Add-Type -assembly "system.io.compression.filesystem"
 
 # URL to download Boost shortened with bit.ly
-# https://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.zip/download
-$download_url = "http://bit.ly/1pk71MX"
-$boost_version = "boost_1_55_0"
+# https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.zip/download
+$download_url = "http://bit.ly/1RbCdbM"
+$boost_version = "boost_1_60_0"
 $filename = $boost_version + ".zip"
 $boost_src_dir = "C:\Boost_Src\"
 $boost_zip_path = $boost_src_dir + $filename
@@ -32,7 +32,7 @@ Write-Host "Building boost.."
 # Boost install command from original script found on AppVeyor support:
 # & .\b2 address-model=64 toolset=msvc-14.0 windows-api=desktop variant=debug link=static threading=multi runtime-link=shared --with-log
 # Command taken from https://github.com/helios-labs-shared/socket.io-client-cpp/blob/master/INSTALL.md#boost_setup
-# Modified to build 64-bit (debug & release) and to use the Visual Studio 12.0 compiler
+# Modified to build 64-bit (debug & release) and to use the Visual Studio 14.0 compiler
 & .\bjam install --prefix=$boost_dir --with-system --with-date_time --with-random link=static runtime-link=shared threading=multi address-model=64 --toolset=msvc-14.0 debug release
 
 popd
--- cmake_generate.ps1
+++ cmake_generate.ps1
@@ -1,2 +1,2 @@
 Write-Host "Generating Visual Studio solution and project files..." 
-& cmake -G "Visual Studio 14 2015 Win64" -DBOOST_ROOT:STRING=C:\Boost -DBOOST_VER:STRING=1.55.0 ./
+& cmake -G "Visual Studio 14 2015 Win64" -DBOOST_ROOT:STRING=C:\Boost -DBOOST_VER:STRING=1.60.0 ./
